ca37e1981417cbdaf2c53c410b27498ae6f0b6e6
80995061-dotcom <80995061@continental.edu.pe>
2026-04-17 17:09:52 -0500

    version2

--- codigo.js
+++ codigo.js
@@ -1,27 +1,27 @@
 const claveApi = '9dad84950e1349bca6e21831260804'
 const idioma ='es' ;
-const inoCity = document.getElementById('input-ciudad')
+const inputCiudad= document.getElementById('input-ciudad')
 
 async function obtenerClima() {
 
-    const city =ipnCity.value;
+    const ciudad =inputCiudad.value;
 
-    if (!city) {
-       alert('por favor,ingrese una city');
+    if (!ciudad) {
+       alert('por favor, ingrese una ciudad');
        return;
     }
 
-    const apliClimaActual =  "httsp://api.weatherapi.com/v1/current.json?q=s{ciudad}&lang=s{idioma}&    key=s {claveApi}"
+    const apliClimaActual =  'https://api.weatherapi.com/v1/current.json?q=s{ciudad}&lang=s{idioma}&    key=s {claveApi}'
 
     const response = await fetch(apliClimaActual);
     const data = await response.json();
 
-    mostraClima(data);
+    mostrarClima(data);
 }
 
 function mostrarClima(data) {
-    document.querySelector('.clima-icono').src =data.current.condition.icon;
-    document.querySelector('.clima-texto').innerHTML =data.current.condition.text;
+    document.querySelector('.clima_icono').src =data.current.condition.icon;
+    document.querySelector('.climaTexto').innerHTML =data.current.condition.text;
     document.querySelector('.temp').innerHTML = data.current.temp_c +'°c';
     document.querySelector('.ciudad').innerHTML = data.location.name;
     document.querySelector('.humedad').innerHTML = data.current.humidity + "%";
